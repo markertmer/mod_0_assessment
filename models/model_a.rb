27cab1506dd7b4108d65d23b292f2777e02c5063
Mark Ertmer <mark.ertmer@gmail.com>
2021-10-21 11:41:53 -0600

    Add DIA hash data

---
+++ models/model_a.rb
@@ -0,0 +1,13 @@
+denver_international_airport = {
+  abbreviation: 'DIA',
+  date_opened: 'February 28, 1995',
+  driving_miles_from_downtown: 24.8,
+  number_of_concourses: 3,
+  has_long_term_parking: true,
+  has_creepy_horse_statue: true,
+  hub_airlines: ['Frontier', 'United'],
+  top_destinations: ['Santa Barbara, CA', 'Miami, FL', 'Anchorage, AK', 'Maui, HI', 'Calgary, Alberta, Canada']
+
+}
+
+puts denver_international_airport

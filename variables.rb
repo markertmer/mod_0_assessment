9b1d8d34174d4383746dd2abe19533b193b7af5a
Mark Ertmer <mark.ertmer@gmail.com>
2021-10-21 11:22:08 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,13 @@
+famous_person = "Macho Man Randy Savage"
+is_active = false
+famous_person = "The Ultimate Warrior"
+
+num_3 = 666
+num_9 = 420
+sum = num_3 + num_9
+
+animals = ['platypus','snow leopard','goat']
+
+animals.shift
+
+puts famous_person.size

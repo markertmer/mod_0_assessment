42a4ab6d73799a215eec2f6a4a8ce8ddd8d864bd
Mark Ertmer <mark.ertmer@gmail.com>
2021-10-21 11:52:54 -0600

    Create my_car hash variable and add key values

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+my_car = {
+  make: "Toyota",
+  model: "Rav4",
+  number_of_doors: 4,
+  avg_miles_per_gallon: 30.2,
+  is_electric: false,
+  has_all_wheel_drive: true,
+  features: ['keyless entry', 'bluetooth audio', 'backup camera', 'alarm system'],
+  issues: ['chipped windshield', 'dent on front passenger door', 'needs a wash']
+}
+
+puts my_car
